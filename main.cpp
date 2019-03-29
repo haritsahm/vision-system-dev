f5def1161855aa2a26c26c9dc4e8097fb56a016c
harit <haritsahm@gmail.com>
2019-03-30 01:36:26 +0700

    add hough line p

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <opencv2/videoio.hpp>
 
 #include <algorithm>
+#include <random>
 
 #define INF 1000000007
 
@@ -19,6 +20,7 @@ using namespace cv;
 string fieldBar = "Field Trackbar";
 string BallBar = "Ball Trackbar";
 string GoalBall = "Goal Trackbar";
+string LineBar = "Line Trackbar";
 string ObstBar = "Obstacle Trackbar";
 
 void saveConfig(const string &path);
@@ -30,6 +32,18 @@ int bins = 64;
 #define GREEN 2
 #define BLACK 3
 #define OTHER 0
+
+Mat image, RawHSVImg, RawBGRImg, undistImage;
+
+
+struct LineSegment
+{
+    double x1,y1;
+    double x2,y2;
+};
+
+struct Vec3 { int x; int y; int z; };
+struct Vec6 {int x; int y; int z; int r; int p; int w;};
 
 
 class hsvRangeC
@@ -45,11 +59,11 @@ public:
 
     void low(Eigen::Vector3d low){h0 = low.x(); s0 = low.y(); v0=low.z();}
     void high(Eigen::Vector3d high){h1 = high.x(); s1 = high.y(); v1=high.z();}
+    void load(Vec6 data){h0 = data.x; h1 = data.y; s0 = data.z; s1 = data.r; v0 = data.p; v1 = data.w;}
 };
 
-struct Vec3 { int x; int y; int z; };
-
 hsvRangeC fieldRange;
+hsvRangeC lineRange;
 
 namespace YAML {
 template<>
@@ -73,11 +87,45 @@ struct convert<Eigen::Vector3d> {
         return true;
     }
 };
+
+template<>
+struct convert<Vec6> {
+    static Node encode(const Vec6& rhs) {
+        Node node;
+        node.push_back(rhs.x);
+        node.push_back(rhs.y);
+        node.push_back(rhs.z);
+        node.push_back(rhs.r);
+        node.push_back(rhs.p);
+        node.push_back(rhs.w);
+        return node;
+    }
+
+    static bool decode(const Node& node,Vec6& rhs) {
+        if(!node.IsSequence() || node.size() != 6) {
+            return false;
+        }
+
+        rhs.x = node[0].as<double>();
+        rhs.y = node[1].as<double>();
+        rhs.z = node[2].as<double>();
+        rhs.r = node[3].as<double>();
+        rhs.p = node[4].as<double>();
+        rhs.w = node[5].as<double>();
+        return true;
+    }
+};
 }
 
 YAML::Emitter& operator << (YAML::Emitter& out, const Eigen::Vector3d& v) {
     out << YAML::Flow;
     out << YAML::BeginSeq << v.x() << v.y() << v.z() << YAML::EndSeq;
+    return out;
+}
+
+YAML::Emitter& operator << (YAML::Emitter& out, const Vec6& v) {
+    out << YAML::Flow;
+    out << YAML::BeginSeq << v.x << v.y << v.z << v.r << v.p << v.w << YAML::EndSeq;
     return out;
 }
 
@@ -119,6 +167,17 @@ void fieldTrackbar()
     createTrackbar("minH", fieldBar, &fieldRange.h0, 255);
 }
 
+void lineTrackbar()
+{
+    namedWindow(LineBar, CV_WINDOW_AUTOSIZE);
+    createTrackbar("minH", LineBar, &lineRange.h0, 255);
+    createTrackbar("maxH", LineBar, &lineRange.h1, 255);
+    createTrackbar("minS", LineBar, &lineRange.s0, 255);
+    createTrackbar("maxS", LineBar, &lineRange.s1, 255);
+    createTrackbar("minV", LineBar, &lineRange.v0, 255);
+    createTrackbar("maxV", LineBar, &lineRange.v1, 255);
+}
+
 vector<Point> contoursConvexHull( vector<vector<Point> > contours )
 {
     vector<Point> result;
@@ -128,7 +187,7 @@ vector<Point> contoursConvexHull( vector<vector<Point> > contours )
         {
             double area = contourArea(contours[i]);
 
-            if(area > 1000)
+            if(area > 700)
                 pts.push_back(contours[i][j]);
         }
     if(pts.size() > 0)
@@ -139,7 +198,7 @@ vector<Point> contoursConvexHull( vector<vector<Point> > contours )
 vector<Point> locateFieldPoints(Mat &fieldBinary)
 {
     vector<Point> fieldPoints;
-    int STEP = 20;
+    int STEP = 10;
     int maxCounter = 100;
 
     for(int col = 0; col < fieldBinary.cols; col+=STEP)
@@ -289,187 +348,185 @@ void scanLine(Mat &input, vector<Point> &linePoint, vector<vector<Point> > &row_
         }
 }
 
-int yBoundaryValue(int x)
-{
 
-}
-
-void findGoalPost(Mat cleanField)
-{
-    int prev_green = 0;
-    int next_green = 0;
-    int counter = 0;
-    bool isgreen = true;
-    vector<Point> goalCandidate;
-    for(int row = 0; row < cleanField.rows; row++)
-    {
-        vector<Point> rowPoint;
-        for(int col = 0; col < cleanField.cols; col++)
-        {
-            int pixel = cleanField.at<uchar>(row,col);
-
-            if(pixel > 0)
-            {
-                isgreen = true;
-                counter = 0;
-            }
-
-            if(pixel == 0 && isgreen == true)
-            {
-                isgreen=false;
-                prev_green = col-1;
-            }
-            else if(pixel ==0 && isgreen == false)
-            {
-                counter++;
-            }
-
-            else if(pixel > 0 && isgreen == false)
-            {
-                isgreen = true;
-                if(counter < cleanField.cols/2)
-                {
-                    counter = 0;
-                    next_green = col-1;
-                    goalCandidate.push_back(Point(col, row));
-                }
-
-            }
-        }
-        //      row_candidate.push_back(rowPoint);
-    }
-
-}
-
-/*
 void saveConfig(const string &path)
 {
-  YAML::Emitter out;
+    YAML::Emitter out;
 
-  Eigen::Vector3d fieldLow = Eigen::Vector3d(fieldRange.h0, fieldRange.s0,fieldRange.v0);
+    Eigen::Vector3d fieldLow = Eigen::Vector3d(fieldRange.h0, fieldRange.s0,fieldRange.v0);
+    Vec6 arp;
+    arp.x = lineRange.h0; arp.y = lineRange.h1; arp.z = lineRange.s0;
+    arp.r = lineRange.s1; arp.p = lineRange.v0; arp.w = lineRange.v1;
 
-  out << YAML::BeginMap;
+    out << YAML::BeginMap;
 
-  out << YAML::Key << "FieldLow" << YAML::Value << fieldLow;
+    out << YAML::Key << "FieldLow" << YAML::Value << fieldLow;
+    out << YAML::Key << "lineVal" << YAML::Value << arp;
 
-  out << YAML::EndMap;
+    out << YAML::EndMap;
 
-  // output to file
-  std::ofstream fout(path.c_str());
-  fout << out.c_str();
+    // output to file
+    std::ofstream fout(path.c_str());
+    fout << out.c_str();
 }
 
 void loadConfig(const string &path)
 {
-  YAML::Node conf = YAML::LoadFile(path.c_str());
+    YAML::Node conf = YAML::LoadFile(path.c_str());
 
-  Eigen::Vector3d fieldLow = conf["FieldLow"].as<Eigen::Vector3d>();
+    Eigen::Vector3d fieldLow = conf["FieldLow"].as<Eigen::Vector3d>();
+    Vec6 linedata = conf["lineVal"].as<Vec6>();
 
-  fieldRange.low(fieldLow);
+    fieldRange.low(fieldLow);
+    lineRange.load(linedata);
 
 }
-*/
+
+
 int main()
 {
     const std::string path = "config.yaml";
 
-    Mat image = imread("lapangan1.jpg", IMREAD_COLOR);
-    //  VideoCapture cap("/home/haritsahm/gazebo.mp4",cv::CAP_FFMPEG );
-    //  if(!cap.isOpened())
-    //  {
-    //    cout << "ERROE" << endl;
-    //    return -1;
-    //  }
+    //    Mat image = imread("lapangan3.jpg", IMREAD_COLOR);
+    VideoCapture cap("gazebo.mp4",cv::CAP_FFMPEG );
+    if(!cap.isOpened())
+    {
+        cout << "ERROE" << endl;
+        return -1;
+    }
 
     cv::Mat camera_matrix = (Mat1d(3, 3) << 296.374353, 0, 240.5, 0, 296.374353, 180.5, 0, 0, 1);
     Mat distortionCoefficients = (Mat1d(1, 5) << 0, 0, 0, 0, 0);
 
     loadLUT("lut.xml");
 
-    //  loadConfig(path);
-    //  fieldTrackbar();
+    loadConfig(path);
+    //    fieldTrackbar();
+    //    lineTrackbar();
 
 
-    //  while(1){
-    Mat RawHSVImg, RawBGRImg, undistImage;
-    //    cap >> image;
-    imshow("Image ", image);
+    while(1){
+        cap >> image;
 
-    RawBGRImg = image;
 
-    cvtColor(RawBGRImg, RawHSVImg, CV_BGR2HSV);
-    std::cout << "huehuehe" << std::endl;
-    /*
+        RawBGRImg = image;
+
+        cvtColor(RawBGRImg, RawHSVImg, CV_BGR2HSV);
+        /*
      * White from LUT
      */
 
-    Mat whiteLUT = Mat::zeros(RawHSVImg.size(), CV_8UC1);
-    //    updateLUT(RawHSVImg, whiteLUT);
+        //        Mat whiteLUT = Mat::zeros(RawHSVImg.size(), CV_8UC1);
+        //        updateLUT(RawHSVImg, whiteLUT);
 
 
-    /*
+        /*
      * Find Contour, get field mask
      */
 
-    Mat fieldBinary = Mat::zeros(RawHSVImg.size(), CV_8UC1);
-    Mat fieldConvectHull = Mat::zeros(RawHSVImg.size(), CV_8UC1);
+        Mat fieldBinary = Mat::zeros(RawHSVImg.size(), CV_8UC1);
+        Mat fieldConvectHull = Mat::zeros(RawHSVImg.size(), CV_8UC1);
 
-    getGreenField(RawBGRImg, fieldBinary);
-    threshold(fieldBinary, fieldBinary, 95, 255, CV_THRESH_BINARY);
+        getGreenField(RawBGRImg, fieldBinary);
+        threshold(fieldBinary, fieldBinary, 97, 255, CV_THRESH_BINARY);
+        Mat element = getStructuringElement(MORPH_ELLIPSE, Size(3,3));
+        erode(fieldBinary, fieldBinary, element);
+
+        erode(fieldBinary, fieldBinary, element);
 
 
-    vector<vector<Point > > contours;
-    findContours(fieldBinary,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-    if(contours.size() > 0)
-    {
-        vector<Point> ConvexHullPoints =  contoursConvexHull(contours);
-        if(ConvexHullPoints.size() > 0)
+        vector<vector<Point > > contours;
+        vector<Point> ConvexHullPoints;
+        findContours(fieldBinary,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE);
+        if(contours.size() > 0)
         {
-            vector<vector<Point> > hullPoints = vector<vector<Point> > (1, ConvexHullPoints);
+            ConvexHullPoints =  contoursConvexHull(contours);
+            if(ConvexHullPoints.size() > 0)
+            {
+                vector<vector<Point> > hullPoints = vector<vector<Point> > (1, ConvexHullPoints);
 
-            //    polylines( RawBGRImg, ConvexHullPoints, true, Scalar(255,0,0), 2 );
-            //    fillPoly(drawing, ConvexHullPoints, Scalar(255,255,255), 2);
-            drawContours(fieldConvectHull, hullPoints, -1, Scalar(255), CV_FILLED);
+                //            polylines( image, ConvexHullPoints, true, Scalar(255,0,0), 2 );
+                //    fillPoly(drawing, ConvexHullPoints, Scalar(255,255,255), 2);
+                drawContours(fieldConvectHull, hullPoints, -1, Scalar(255), CV_FILLED);
+            }
         }
-    }
 
-    /*
-     * Field Edge Point
-     */
+        /*
+        * Field Edge Point
+        */
 
-    Mat cleanField;
-    cv::bitwise_and(fieldBinary, fieldConvectHull, cleanField);
-    vector<Point> fieldBoundary = locateFieldPoints(cleanField);
+        Mat cleanField;
+        cv::bitwise_and(fieldBinary, fieldConvectHull, cleanField);
+        vector<Point> fieldBoundary = locateFieldPoints(cleanField);
+        //    Vec4d fieldLines; Point pt1, pt2;
 
-    for(int i = 0; i < fieldBoundary.size(); i++)
-    {
-        circle(image, fieldBoundary[i], 3, Scalar(100,255,100));
-    }
+        //    for(int i = 0; i < fieldBoundary.size(); i++)
+        //    {
+        //        circle(image, fieldBoundary[i], 3, Scalar(100,255,100));
+        //    }
 
-    /*
+
+        /*
      * Field Line Poitns
      */
 
-    Mat inv_field = Mat::zeros(cleanField.size(), CV_8UC1);
-    bitwise_not(cleanField, inv_field);
-    multiply(inv_field,fieldConvectHull,inv_field);
-    vector<Point> linePoints;
-    vector<vector<Point> > row_candidate, col_candidate;
+        Mat inv_field = Mat::zeros(cleanField.size(), CV_8UC1);
+        bitwise_not(cleanField, inv_field);
+        multiply(inv_field,fieldConvectHull,inv_field);
 
-    scanLine(inv_field, linePoints, row_candidate, col_candidate);
+        //    cv::bitwise_and()
 
-    for(int i = 0; i < linePoints.size(); i++)
-        circle(image, linePoints[i], 3, Scalar(100,100,255));
 
-    //        imshow("Convect Hull", fieldConvectHull);
-    imshow("Image ", image);
-    imshow("Binary", cleanField);
+        //    vector<Point> linePoints;
+        //    vector<vector<Point> > row_candidate, col_candidate;
 
-    //    if(waitKey(30) == 'q') break;
-    //    else if(waitKey(30) == 's') saveConfig(path);
+        //    scanLine(inv_field, linePoints, row_candidate, col_candidate);
 
-    waitKey();
-    //  }
+        //    for(int i = 0; i < linePoints.size(); i++)
+        //        circle(image, linePoints[i], 3, Scalar(100,100,255));
+
+
+        /*
+       * Field Line
+       */
+        Mat out, lines_img;
+        Mat lines_out;
+        Mat set = image.clone();
+        inRange(RawHSVImg, Scalar(lineRange.h0, lineRange.s0, lineRange.v0), Scalar(lineRange.h1, lineRange.s1, lineRange.v1), out);
+        bitwise_and(inv_field, out, lines_img);
+        set.copyTo(lines_out, lines_img);
+
+        cvtColor(lines_out, lines_out, CV_BGR2GRAY);
+        blur( lines_out, lines_out, Size(3,3) );
+
+        /// Canny detector
+        Canny( lines_out, lines_out, 100, 300, 3 );
+        Mat canny_out =lines_out.clone();
+
+        Mat cdstP = image.clone();
+
+        // Probabilistic Line Transform
+        vector<Vec4i> linesP; // will hold the results of the detection
+        HoughLinesP(lines_out, linesP, 1, CV_PI/180, 50, 50, 10 ); // runs the actual detection
+        // Draw the lines
+        for( size_t i = 0; i < linesP.size(); i++ )
+        {
+            Vec4i l = linesP[i];
+            line( cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+        }
+
+        imshow("clean_white", cleanField);
+        imshow("Image ", image);
+        imshow("inv_field", inv_field);
+        imshow("line hsv", cdstP);
+        imshow("canny", canny_out);
+        imshow("mask", cdstP);
+
+        if(waitKey(30) == 27) break;
+        else if(waitKey(30) == 's') saveConfig(path);
+
+        //        waitKey();
+    }
 
     return 0;
 }
